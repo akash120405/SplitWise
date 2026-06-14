1641b98481b99ebdd3c3da328e1f9138439c0297
V Akash <v.akash23@lpu.in>
2026-06-14 20:27:16 +0530

    Remove compiled JS files from repository

--- src/routes/expense.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const expense_controller_1 = require("../controllers/expense.controller");
-const auth_middleware_1 = require("../middleware/auth.middleware");
-const router = (0, express_1.Router)();
-router.post("/", auth_middleware_1.protect, expense_controller_1.createExpense);
-router.get("/balances/:groupId", auth_middleware_1.protect, expense_controller_1.getBalances);
-exports.default = router;
--- src/routes/settlement.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const settlement_controller_1 = require("../controllers/settlement.controller");
-const router = (0, express_1.Router)();
-router.get("/:groupId/settlements", settlement_controller_1.getSettlements);
-exports.default = router;
